97af27fa9ac2cc3b8f7e37da349c709f992bf124
Kevin Muscara <muscarak1@nku.edu>
2026-02-08 19:24:49 -0500

    added flyout filtering to redirect to page

--- content.js
+++ content.js
@@ -5,3 +5,48 @@ document.addEventListener('keydown', function(event) {
     document.querySelector('[data-testid=btn-global-search]').click();
   }
 });
+
+let previousUrl = window.location.href;
+
+function checkAndRedirectFlyout() {
+  const params = new URLSearchParams(window.location.search);
+  const flyoutType = params.get('flyout-type');
+  const flyoutId = params.get('flyout-id');
+  
+  if (flyoutType && flyoutId) {
+    if (flyoutType === 'asset') {
+      window.location.replace(`/agent/assets/${flyoutId}`);
+    } else if (flyoutType === 'agent-ticket') {
+      window.location.replace(`/agent/tickets/${flyoutId}`);
+    } else if (flyoutType === 'agent-user') {
+      window.location.replace(`/agent/users/${flyoutId}`);
+    } else {
+      console.log('Unknown flyout type, allowing flyout to open:', flyoutType);
+    }
+  }
+}
+
+checkAndRedirectFlyout();
+
+const originalPushState = window.history.pushState;
+window.history.pushState = function(...args) {
+  const result = originalPushState.apply(this, args);
+  setTimeout(checkAndRedirectFlyout, 0);
+  return result;
+};
+
+const originalReplaceState = window.history.replaceState;
+window.history.replaceState = function(...args) {
+  const result = originalReplaceState.apply(this, args);
+  setTimeout(checkAndRedirectFlyout, 0);
+  return result;
+};
+
+window.addEventListener('popstate', () => setTimeout(checkAndRedirectFlyout, 0));
+
+setInterval(() => {
+  if (window.location.href !== previousUrl) {
+    previousUrl = window.location.href;
+    checkAndRedirectFlyout();
+  }
+}, 100);
